d631745a1483c71511f9edf46d842f999aa4af24
s7k-windows <benotimprotant@mail.ru>
2026-09-18 00:31:17 +0300

    Minio bucket initialization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import config
 from app import app
 from database import init_db
+from s3 import s3
 
 def main():
     init_db()
+    s3.init_public_bucket()
     
     app.run(host='0.0.0.0', port=config.FLASK_PORT, debug=True)
 
